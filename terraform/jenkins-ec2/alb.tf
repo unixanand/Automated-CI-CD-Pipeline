6a3db295191ce3d1ff054fbc4dc3427e340d4eb2
unix_anand <unixanand2005@gmail.com>
2026-02-26 12:30:20 +0000

    SG fix

--- terraform/jenkins-ec2/alb.tf
+++ terraform/jenkins-ec2/alb.tf
@@ -1,6 +1,6 @@
 resource "aws_lb" "app_alb" {
   name               = "streamlit-alb"
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.k8s_sg.id]
+  security_groups    = [aws_security_group.alb_sg.id]
   subnets            = data.aws_subnets.default.ids
 }
